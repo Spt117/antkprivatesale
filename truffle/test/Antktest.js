22a0e0760022328d05c198603a6cee4f6cb6e1d5
jb <theshitcoinert@gmail.com>
2022-09-21 03:35:07 +0200

    majtest

--- truffle/test/Antktest.js
+++ truffle/test/Antktest.js
@@ -13,9 +13,11 @@ contract('AntkPrivateTest', accounts => {
     const whitelist1 = '0x39C4Ce965b9F81de90D5F91127afB807bdd1F705';
     const whitelist2 = '0x311EEfAE053Eeb0fB77C750Ee3d84dFd88B2c5b5';
     const other = '0x1dCDc0A79dac4ff8Ed895453dFE89402f7248D04';
-    const root = '0x13175a325706e00569533fdcb6cc5014cdfdf56aeecabd25613b7cff497937cc';
+    const root = '0x593536617a764d87868776ad17abb710e27751f667631ad29751cbcf02a6c12d';
     const merkleProof = [0xe4506196644de3e89c3c54b222159d85ec4f190db43b8f73717d0b6d999e5b74,0x2051b23223f6d5d83040309ebe1f9910538fd0d2137237f11cba78178cc7bd37,0xecf69cabc20f2ae0792c63c8208a565313fea1aa62f2c2f36089554a036fce67,0xbb266e6e62b2d273fcc37081216848aec7b90478646fafb132f58fd57271751b,0x08b60d69d596a6ce1e54ff87bdf9e390a4319b701a25bb8abdec4e67ee248487]
-
+    const merkleProof2 = [
+        '0x317ad114c8852985eba29964133925507984e84cacaed9e4e7f78bcbe481b4ec'
+      ]
     const goerliEthChainlink = '0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e';
 
     let AntkPrivateInstance;
@@ -31,16 +33,16 @@ contract('AntkPrivateTest', accounts => {
         })
         console.log(merkleProof)
         it("...should revert because of Ownable", async () => {
-            await expectRevert(AntkPrivateInstance.setRoot(merkleProof, { from: other }), "Ownable: caller is not the owner");
+            await expectRevert(AntkPrivateInstance.setRoot(root, { from: other }), "Ownable: caller is not the owner");
         });
 
         it("...should return true", async () => {
-            const isWhitelisted = await AntkPrivateInstance.isWhitelist(merkleProof, {from: whitelist1})
+            const isWhitelisted = await AntkPrivateInstance.isWhitelist(merkleProof2, {from: whitelist1})
             expect(isWhitelisted).to.be.true
         })
 
         it("...should return false", async () => {
-            const isWhitelisted = await AntkPrivateInstance.isWhitelist(merkleProof, {from: ganache})
+            const isWhitelisted = await AntkPrivateInstance.isWhitelist(merkleProof2, {from: ganache})
             expect(isWhitelisted).to.be.false
         })
 
